9742fa9ff792d58fa6c7f67963ce9f20b483b25b
Jonathan Doherty <dohjon@users.noreply.github.com>
2021-12-15 10:57:35 +0100

    feat: add tailwindcss

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+
+import '../styles/globals.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,11 +10,6 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to site!</title>
       </Head>
       <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to site!</h1>
-        </header>
         <main>
           <Component {...pageProps} />
         </main>
