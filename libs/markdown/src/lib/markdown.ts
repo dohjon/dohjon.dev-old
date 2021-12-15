6020ed6b81486ede899f963371be7918b0586eff
Jonathan Doherty <dohjon@users.noreply.github.com>
2021-12-15 15:30:52 +0100

    feat: add shared library mdx-elements

--- libs/markdown/src/lib/markdown.ts
+++ libs/markdown/src/lib/markdown.ts
@@ -1,3 +1,29 @@
-export function markdown(): string {
-  return 'markdown';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import * as matter from 'gray-matter';
+import { serialize } from 'next-mdx-remote/serialize';
+
+interface MarkdownContent {
+  frontMatter: {
+    [key: string]: unknown;
+  };
+  content: string;
+}
+
+export function getParsedFileContentBySlug(
+  fileName: string,
+  postsPath: string
+) {
+  const postFilePath = join(postsPath, `${fileName}.mdx`);
+  const fileContent = readFileSync(postFilePath);
+
+  const { data, content } = matter(fileContent);
+  return {
+    frontMatter: data,
+    content,
+  } as MarkdownContent;
+}
+
+export function renderMarkdown(markdownContent: string) {
+  return serialize(markdownContent);
 }
